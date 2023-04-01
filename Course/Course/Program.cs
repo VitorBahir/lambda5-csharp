aa623cd3998df059116daa166a1224a2a84f42e7
Vitor Bahir de Andrade <105059969+VitorBahir@users.noreply.github.com>
2023-04-01 15:49:35 -0300

    Initial implementation

---
+++ Course/Course/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+using Course.Entities;
+
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!(sr.EndOfStream))
+                    {
+                        string[] fields = sr.ReadLine().Split(",");
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        list.Add(new Product(name, price));
+                    }
+                }
+                var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+                Console.WriteLine("Average price: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+                var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+                foreach (string name in names)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+        }
+    }
+}
